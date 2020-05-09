c90a2af084215f51a874253b4cf48e3330ed95be
kevinsims1 <kevinsims1@outlook.com>
2020-05-09 12:58:13 -0700

    added form to recriverSignUp

--- src/pages/receiverSignUp.js
+++ src/pages/receiverSignUp.js
@@ -1,10 +1,26 @@
 import React from 'react' 
+import {Box, Input, Label} from 'theme-ui'
 
 const Receiver = () => {
   return(
-    <>
-      <h1>Hello</h1>
-    </>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        as='form'
+        // onSubmit={e => e}
+      >
+        <Label>First Name</Label>
+          <Input 
+            type='name'
+            name='first name'
+          />
+      </Box>
+    </Box>
   )
 }
 
